16f13f459ca367b24ae924f41fa13de88d210bc7
Joel Warwick <60834182+jwarwick03@users.noreply.github.com>
2025-03-13 16:21:46 +0000

    exercise 9-2 finished

--- exercise 9-2/slide_show.js
+++ exercise 9-2/slide_show.js
@@ -4,8 +4,8 @@ $(document).ready( () => {
         
     // start slide show
     setInterval( () => {   
-        $("#caption").fadeOut(1000);
-        $("#slide").fadeOut(1000,
+        $("#caption").hide(1000);
+        $("#slide").slideUp(2000,
             () => {
                 if (nextSlide.next().length == 0) {
                     nextSlide = $("#slides img:first-child");
@@ -15,9 +15,9 @@ $(document).ready( () => {
                 }
                 const nextSlideSource = nextSlide.attr("src");
                 const nextCaption = nextSlide.attr("alt");
-                $("#slide").attr("src", nextSlideSource).fadeIn(1000);                    
-                $("#caption").text(nextCaption).fadeIn(1000);
+                $("#slide").attr("src", nextSlideSource).slideDown(2000);                    
+                $("#caption").text(nextCaption).show(1000);
             });    // end fadeOut()
     },
-    3000);         // end setInterval()
+    5000);         // end setInterval()
 });
